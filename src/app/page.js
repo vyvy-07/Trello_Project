d4a213c3128c3073fb0db9c874335189f2fe1146
vyvy-07 <nguyenthuyvy12all@gmail.com>
2024-04-12 15:50:30 +0700

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,29 @@
+'use client';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { CssBaseline, ThemeProvider, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import DepartureBoardIcon from '@mui/icons-material/DepartureBoard';
 import CloudDoneSharpIcon from '@mui/icons-material/CloudDoneSharp';
-import { CssBaseline } from '@mui/material';
-
+import theme from './theme';
 export default function Home() {
   return (
-    <main>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div>Hello World!</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br />
-      <DepartureBoardIcon />
-      <CloudDoneSharpIcon />
-    </main>
+      <main>
+        <Typography variant="body2" color="text.secondary">
+          btn Typography
+        </Typography>{' '}
+        <div>Hello World!</div>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+        <br />
+        <DepartureBoardIcon />
+        <CloudDoneSharpIcon />
+      </main>{' '}
+    </ThemeProvider>
   );
 }
